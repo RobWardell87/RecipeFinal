70e4ab41e7777b7a5b38eb35c22a0ebb4eeaa60f
RobWardell87 <r.wardell1987@gmail.com>
2026-01-19 19:27:58 -0500

    ModalBug

---
+++ index.js
@@ -0,0 +1,180 @@
+const SEARCH_API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+const RANDOM_API_URL = "https://www.themealdb.com/api/json/v1/1/random.php"
+const LOOKUP_API_URL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
+
+const searchForm = document.getElementById("search__form")
+const searchInput = document.getElementById("search__input")
+const resultsGrid = document.getElementById("results__grid")
+const messageArea = document.getElementById("message__area")
+const randomButton = document.getElementById("random__button")
+const modal = document.getElementById("recipe__modal")
+const modalContent = document.getElementById("recipe__details--content")
+const modalCloseBtn = document.getElementById("modal__close--btn")
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const searchTerm = searchInput.value.trim()
+
+  if (searchTerm) {
+    searchRecipes(searchTerm)
+  } 
+  else {
+    showMessage("Please enter a search term", true)
+  }
+})
+
+async function searchRecipes(query) {
+  showMessage(`Searching for "${query}"...`, false, true)
+  resultsGrid.innerHTML = ""
+
+  try {
+    const response = await fetch(`${SEARCH_API_URL}${query}`)
+    if (!response.ok) throw new Error("Network error")
+    const data = await response.json()
+    clearMessage()
+    console.log("data: ", data)
+
+    if (data.meals) {
+      displayRecipes(data.meals)
+    } 
+    else {
+      showMessage(`No recipes found for "${query}",`)
+    }
+  } 
+    catch (error) {
+      showMessage("Something went wrong, Please try again.", true)
+  }
+}
+
+function showMessage(message, isError = false, isLoading = false) {
+  messageArea.textContent = message
+  if (isError) messageArea.classList.add("error")
+  if (isLoading) messageArea.classList.add("loading")
+}
+
+function clearMessage() {
+  messageArea.textContent = ""
+  messageArea.className = "message"
+}
+
+function displayRecipes(recipes) {
+    if (!recipes || recipes.length === 0) {
+        showMessage("No recipes to display")
+        return
+    }
+    recipes.forEach(recipe => {
+        const recipeDiv = document.createElement("div")
+        recipeDiv.classList.add("recipe__item")
+        recipeDiv.dataset.id = recipe.idMeal
+        recipeDiv.innerHTML = `
+        <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}" loading="lazy">
+        <h3>${recipe.strMeal}</h3>
+        `
+        resultsGrid.appendChild(recipeDiv)
+    })
+}
+
+randomButton.addEventListener("click", getRandomRecipe)
+
+async function getRandomRecipe() {
+    showMessage("fetching a random recipe...", false, true)
+    resultsGrid.innerHTML = ""
+    try {
+        const response = await fetch(RANDOM_API_URL)
+        if (!response.ok) throw new Error("something went wrong.")
+        const data = await response.json()
+
+        clearMessage()
+        if (data.meals && data.meals.length > 0){
+            displayRecipes(data.meals)
+        }
+        else {
+            showMessage("could not fetch a random recipe. Please try again.", true)
+        }
+    } 
+    catch (error) {
+        showMessage("Failed to fetch a random recipe. Please check your connection and try again.", true)
+    }
+}
+
+function showModal() {
+    modal.classList.remove("hidden")
+    document.body.style.overflow = "hidden"
+}
+
+function closeModal() {
+    modal.classList.add("hidden")
+    document.body.overflow = ""
+}
+
+resultsGrid.addEventListener("click", (e) => {
+    const card = e.target.closest(".recipe__item")
+
+    if (card) {
+        const recipeId = card.dataset.id
+        getRecipeDetails(recipeId)
+    }
+})
+
+async function getRecipeDetails(id) {
+    modalContent.innerHTML = '<p class="message loading">Loading details...</p>'
+    showModal()
+
+    try {
+       const response = await fetch(`${LOOKUP_API_URL}${id}`) 
+       if (!response.ok) throw new Error("Failed to fetch recipe details.")
+       const data = await response.json()
+       if (data.meals && data.meals > 0) {
+        displayRecipeDetails(data.meals[0])
+       }
+       else {
+        modalContent.innerHTML = '<p class="message error">Could not load recipe details.</p>'
+       }
+    } 
+    catch (error) {
+       modalContent.innerHTML = '<p class="message error">Failed to load recipe details. Please check your connection or try again.</P>' 
+    }
+}
+
+modalCloseBtn.addEventListener("click", closeModal)
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        closeModal()
+    }
+})
+
+function displayRecipeDetails(recipe) {
+    const ingredients = []
+
+    for (let i = 1; i <= 20; ++i) {
+        const ingredient = recipe[`strIngredients${i}`]?.trim()
+        const measure = recipe[`strMeasure${i}`]?.trim()
+
+        if (ingredient) {
+            ingredients.push(`<li>${measure ? `${measure}` : ""}${ingredient}</li>`)
+        }
+        else {
+         break;   
+        }
+    }
+
+    const categoryHTML = recipe.strCategory ? `<h3>Category: ${recipe.strCategory}</h3>` : ""
+    const areaHTML = recipe.strArea ? `<h3>Area: ${recipe.strArea}</h3>` : ""
+    const ingredientsHTML = ingredients.length ? `<h3>Ingredients:</h3><ul>${ingredients.join("")}</ul>` : ""
+    const instructionsHTML = `<h3>Instructions:</h3><p>${recipe.strInstructions ? 
+    recipe.strInstructions.replace(/\r?\n/g, "<br>") : "Instructions not available."}</p>` 
+    const youtubeHTML = recipe.strYoutube ? `<h3>Video Recipe</h3><div class="video__wrapper"><a href="${recipe.strYoutube}"
+    target="_blank">Watch on YouTube.</a></div>`  : "" 
+    const sourceHTML = recipe.strSource ? `<div class="source__wrapper"><a href="${recipe.strSource}" target="_blank">View Original Source.</a></div>` : ""
+    
+    modalContent.innerHTML = `
+    <h2>${recipe.strMeal}</h2>
+    <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+    ${categoryHTML}
+    ${areaHTML}
+    ${ingredientsHTML}
+    ${instructionsHTML}
+    ${youtubeHTML}
+    ${sourceHTML}`
+}
